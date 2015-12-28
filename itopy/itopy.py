f621a90bc03a5b040ad5a9200fa57ac8a8eb485d
Jonatas Baldin <jonatas.baldin@gmail.com>
2015-12-28 11:19:58 -0200

    add decorator to authenticate

--- itopy/itopy.py
+++ itopy/itopy.py
@@ -85,6 +85,24 @@ class Api(object):
                 # Returns just custom exception message
                 return e.args[0]
 
+    def auth(func):
+        """
+        Decorator to authenticate the functions that must be authenticated
+        Parameters:
+        func: function to authenticate
+        """
+        # gets the function and all its parameters
+        def inner(self, *args, **kwargs):
+            # verify if self.auth is 0 (auththenticated) or not
+            if self.auth == 0:
+                # if it ts, return the function with normal request
+                return func(self, *args, **kwargs)
+            else:
+                # if not, returns that it is not authenticated
+                return self.connect_error(self.auth)
+        # returns the inner function
+        return inner
+
     def connect_error(self, error):
         """
         Return connection error, if any.
@@ -112,7 +130,7 @@ class Api(object):
             return error_dict[error]
         else:
             return 'UNKNOW_ERROR - Not specified by ITOP'
-
+    @auth
     def req(self, data, obj_class):
         """
         Gereral request to iTOP API.
@@ -121,61 +139,56 @@ class Api(object):
         data: JSON structure data, in dict
         ojb_class: iTOP's device class from datamodel
         """
-        if not hasattr(self, 'auth'):
-            return 'Not authenticated'
+        json_data = json.dumps(data)
+        #json_data = jsonpickle.encode(data)
 
-        if self.auth == 0:
-            # Converts dict to json object
-            json_data = json.dumps(data)
-            #json_data = jsonpickle.encode(data)
+        # Multiple  exceptions
+        schema_exceptions = (requests.exceptions.MissingSchema,
+            requests.exceptions.InvalidSchema)
 
-            # Multiple  exceptions
-            schema_exceptions = (requests.exceptions.MissingSchema,
-                requests.exceptions.InvalidSchema)
+        # Tries web request
+        try:
+            req = requests.post(self.url, data={'version': self.version,
+                'auth_user': self.auth_user, 'auth_pwd': self.auth_pwd,
+                'json_data': json_data})
+        except schema_exceptions  as e:
+            return 'The http:// is missing or invalid'
+        except requests.exceptions.ConnectionError as e:
+            return 'The connection to iTOP was refused'
 
-            # Tries web request
-            try:
-                req = requests.post(self.url, data={'version': self.version,
-                    'auth_user': self.auth_user, 'auth_pwd': self.auth_pwd,
-                    'json_data': json_data})
-            except schema_exceptions  as e:
-                return 'The http:// is missing or invalid'
-            except requests.exceptions.ConnectionError as e:
-                return 'The connection to iTOP was refused'
+        try:
+            json_return = json.loads(req.content.decode('utf-8'))
+        except ValueError as e:
+            return 'Not a valid JSON, maybe the page is returning other data'
 
-            try:
-                json_return = json.loads(req.content.decode('utf-8'))
-            except ValueError as e:
-                return 'Not a valid JSON, maybe the page is returning other data'
+        # The return is 'bytes', convert to string
+        return_code = json_return['code']
 
-            # The return is 'bytes', convert to string
-            return_code = json_return['code']
+        # Default return_list 
+        return_list = {
+                        'code': json_return['code'],
+                        'message': json_return['message'],
+                      }
 
-            # Default return_list 
-            return_list = {
-                            'code': json_return['code'],
-                            'message': json_return['message'],
-                          }
+        # if there's no error, returns +objects and +item_key
+        if return_code == 0:
+            # create a dict key for the items
+            # useful for Get
+            item_key = None
+            temp_dict = (json_return['objects'])
+            item_key = list()
+            if temp_dict is not None:
+                for key in json_return['objects']:
+                    item_key.append(json_return['objects'][key]['key'])
+                
+                # old comprehension..
+                #item_key.append(int([objects[p]['key'] for p in objects][0]))
 
-            # if there's no error, returns +objects and +item_key
-            if return_code == 0:
-                # create a dict key for the items
-                # useful for Get
-                item_key = None
-                temp_dict = (json_return['objects'])
-                item_key = list()
-                if temp_dict is not None:
-                    for key in json_return['objects']:
-                        item_key.append(json_return['objects'][key]['key'])
-                    
-                    # old comprehension..
-                    #item_key.append(int([objects[p]['key'] for p in objects][0]))
+            # adds objects and item_key do dict
+            return_list['objects'] = json_return['objects']
+            return_list['item_key'] = item_key
 
-                # adds objects and item_key do dict
-                return_list['objects'] = json_return['objects']
-                return_list['item_key'] = item_key
-
-            return return_list
+        return return_list
 
     def check_class(self, obj_class):
         """
@@ -199,6 +212,7 @@ class Api(object):
         else:
             return 'name'
 
+    @auth
     def get(self, obj_class, key, output_fields='*'):
         """
         Handles the core/get operation in iTOP.
@@ -207,21 +221,19 @@ class Api(object):
         key: search filter in iTOP's datamodel
         output_fields: fields to get from iTOP's response, defaults is name
         """
-        if not hasattr(self, 'auth') or self.auth == 1:
-            return 'Not authenticated'
 
-        if self.auth == 0:
-            data = {
-                'operation': 'core/get',
-                'comment': 'Get' + obj_class,
-                'class': obj_class,
-                'key': key,
-                'output_fields': output_fields
-            }
+        data = {
+            'operation': 'core/get',
+            'comment': 'Get' + obj_class,
+            'class': obj_class,
+            'key': key,
+            'output_fields': output_fields
+        }
 
-            request = self.req(data, obj_class)
-            return request
+        request = self.req(data, obj_class)
+        return request
 
+    @auth
     def delete(self, obj_class, **kwargs):
         """
         Handles the core/delete operation in iTOP.
@@ -229,27 +241,25 @@ class Api(object):
         ojb_class: iTOP's device class from datamodel
         **kwargs: any field from the datamodel to identify the object
         """
-        if not hasattr(self, 'auth'):
-            return 'Not authenticated'
 
-        if self.auth == 0:
-            data = {
-                'operation': 'core/delete',
-                'comment': 'Delete' + obj_class,
-                'class': obj_class,
-                'key': {
-                }
+        data = {
+            'operation': 'core/delete',
+            'comment': 'Delete' + obj_class,
+            'class': obj_class,
+            'key': {
             }
+        }
 
-            for key, value in kwargs.items():
-                if value:
-                    data['key'][key] = value
-                if not value:
-                    return 'Parameter not valid!'
+        for key, value in kwargs.items():
+            if value:
+                data['key'][key] = value
+            if not value:
+                return 'Parameter not valid!'
 
-            request = self.req(data, obj_class)
-            return request
+        request = self.req(data, obj_class)
+        return request
 
+    @auth
     def create(self, obj_class, output_fields='*', **kwargs):
         """
         Handles the core/create operation in iTOP.
@@ -260,66 +270,60 @@ class Api(object):
             some fields, like brand_name is not added without its id, brand_id,
             it is recommended to use just brand_id, in that case
         """
-        # verify if authentication was made
-        if not hasattr(self, 'auth'):
-            return 'Not authenticated'
 
-        # verify if authentication was successful
-        if self.auth == 0:
+        # verifies if object exists
+        # filter to get the object
+        # unless dontcheck, those objects may have duplicates
+        obj_field = self.check_class(obj_class)
+        if obj_field == 'dontcheck':
+            obj = dict()
+            obj['message'] = 'Found: 0'
+            obj['code'] = 0
+        else:
+            get_filter = "SELECT {} WHERE {} = '{}'".format(obj_class, obj_field, kwargs[obj_field])
+            # actual request
+            obj = self.get(obj_class, get_filter)
 
-            # verifies if object exists
-            # filter to get the object
-            # unless dontcheck, those objects may have duplicates
-            obj_field = self.check_class(obj_class)
-            if obj_field == 'dontcheck':
-                obj = dict()
-                obj['message'] = 'Found: 0'
-                obj['code'] = 0
-            else:
-                get_filter = "SELECT {} WHERE {} = '{}'".format(obj_class, obj_field, kwargs[obj_field])
-                # actual request
-                obj = self.get(obj_class, get_filter)
-
-            # if found 0 objects, and the return code is ok
-            if obj['message'] == 'Found: 0' and obj['code'] == 0:
-                data = {
-                    'operation': 'core/create',
-                    'comment': 'Create' + obj_class,
-                    'class': obj_class,
-                    'output_fields': output_fields,
-                    'fields': {
-                    }
+        # if found 0 objects, and the return code is ok
+        if obj['message'] == 'Found: 0' and obj['code'] == 0:
+            data = {
+                'operation': 'core/create',
+                'comment': 'Create' + obj_class,
+                'class': obj_class,
+                'output_fields': output_fields,
+                'fields': {
                 }
+            }
 
-                # when creating DocumentFile, file parameter is specified
-                # it's a reserved word, so in the request we use _file
-                # and translate here to a key 'file'
-                if '_file' in kwargs:
-                    for item in kwargs['_file']:
-                        kwargs['file'] = item
-                    del kwargs['_file']
+            # when creating DocumentFile, file parameter is specified
+            # it's a reserved word, so in the request we use _file
+            # and translate here to a key 'file'
+            if '_file' in kwargs:
+                for item in kwargs['_file']:
+                    kwargs['file'] = item
+                del kwargs['_file']
 
-                # do not allow empty values in parameters
-                for key, value in kwargs.items():
-                    if value:
-                        data['fields'][key] = value
-                    if not value:
-                        return 'Parameter not valid'
+            # do not allow empty values in parameters
+            for key, value in kwargs.items():
+                if value:
+                    data['fields'][key] = value
+                if not value:
+                    return 'Parameter not valid'
 
-                request = self.req(data, obj_class)
-                return request
+            request = self.req(data, obj_class)
+            return request
 
-            # if not 'Found: 0', but found something, obj exists
-            elif 'Found' in obj['message']:
-                return {
-                    'message': 'Object exists',
-                    'code': 0
-                }
-            else:
-                # if found is not present in string, probably error
-                return obj
+        # if not 'Found: 0', but found something, obj exists
+        elif 'Found' in obj['message']:
+            return {
+                'message': 'Object exists',
+                'code': 0
+            }
+        else:
+            # if found is not present in string, probably error
+            return obj
 
-
+    @auth
     def update(self, obj_class, key, key_value, output_fields='*', **kwargs):
         """
         Handles the core/update operation in iTOP.
@@ -333,35 +337,25 @@ class Api(object):
             some fields, like brand_name is not added without its id, brand_id,
             it is recommended to use just brand_id, in that case
         """
-        # verify if authentication was made
-        if not hasattr(self, 'auth'):
-            return 'Not authenticated'
 
-        # verify if authentication was successful
-        if self.auth == 0:
+        data = {
+            'operation': 'core/update',
+            'comment': 'Create' + obj_class,
+            'class': obj_class,
+            'output_fields': output_fields,
+            'fields': {
+            },
+            'key': {
+                key: key_value
+             }
+        }
 
-            data = {
-                'operation': 'core/update',
-                'comment': 'Create' + obj_class,
-                'class': obj_class,
-                'output_fields': output_fields,
-                'fields': {
-                },
-                'key': {
-                    key: key_value
-                 }
-            }
+        # do not allow empty values in parameters
+        for kkey, kvalue in kwargs.items():
+            if kvalue:
+                data['fields'][kkey] = kvalue
+            if not kvalue:
+                return 'Parameter not valid'
 
-            # do not allow empty values in parameters
-            for kkey, kvalue in kwargs.items():
-                if kvalue:
-                    data['fields'][kkey] = kvalue
-                if not kvalue:
-                    return 'Parameter not valid'
-
-            request = self.req(data, obj_class)
-            return request
-
-        else:
-            # if found is not present in string, probably error
-            return obj
+        request = self.req(data, obj_class)
+        return request
